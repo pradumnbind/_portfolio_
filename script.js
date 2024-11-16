4546031a30f824fb89efe6f7beb2c853f6930332
Pradumn Kumar <pradumnbind66@gmail.com>
2024-11-16 11:21:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,158 @@
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+
+    if (menuBtn.className === "nav-menu"){
+        menuBtn.className += " responsive";
+    } else{
+        menuBtn.className = "nav-menu";
+    }
+}
+
+
+// Dark Mode
+
+
+const body = document.querySelector("body"),
+  toggleSwitch = document.getElementById("toggle-switch");
+
+  toggleSwitch.addEventListener("click", ()=> {
+    body.classList.toggle("dark");
+  });
+
+
+
+//Typed Effects
+
+
+var typingEffect = new Typed(".typedText", {
+    strings: ["PradumnKumar", "Developer", "Coder"],
+
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 2000,
+
+})
+
+
+//Scroll Animation
+
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+});
+
+sr.reveal(".featured-name", { delay: 100 });
+sr.reveal(".text-info", { delay: 200 });
+sr.reveal(".text-btn", { delay: 200 });
+sr.reveal(".social_icons", { delay: 200 });
+sr.reveal(".featured-image", { delay: 320 });
+
+
+sr.reveal(".project-box", { interval: 200});
+
+sr.reveal(".top-header", {});
+
+const srLeft = ScrollReveal({ 
+    origin: "left",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+})
+
+srLeft.reveal(".about-info", { delay: 100});
+srLeft.reveal(".contact-info", { delay: 100});
+srLeft.reveal(".form-control", { delay: 100});
+
+const srRight = ScrollReveal({ 
+    origin: "left",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+})
+
+srRight.reveal(".skill", { delay: 100});
+srRight.reveal(".skill-box", { delay: 100});
+
+
+//Active Link
+
+const sections = document.querySelectorAll(".section[id]");
+
+function scrollActive() {
+    sections.forEach((current) => {
+
+        const sectionHeight = current.offsetHeight,
+           
+            sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document
+            .querySelector(".nav-menu a[href*=" + sectionId + "]")
+            .classList.add("active-link");
+        } else {
+            document
+            .querySelector(".nav-menu a[href*=" + sectionId + "]")
+            .classList.remove("active-link");
+        }
+
+    });
+}
+
+window.addEventListener("scroll", scrollActive);
+
+
+// contact form
+
+$('#submit_form').submit(function(e){
+    e.preventDefault();
+
+    if(!$('input[name=name]').val()){
+        alert('Name field is required.');
+        return;
+    }
+
+    if(!$('input[name=email]').val()){
+        alert('Email field is required.');
+        return;
+    }
+
+    if(!$('input[name=subject]').val()){
+        alert('Subject field is required.');
+        return;
+    }
+
+    if(!$('textarea[name=message]').val()){
+        alert('Message field is required.');
+        return;
+    }
+
+    $('#success_message').show();
+
+    alert('Thank you! Your message has been successfully submitted. I will be in touch with you soon.');
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
